16b16acfb008793df39c4a741953a8805877bd40
Ashish Kapoor <swiftobjc@gmail.com>
2017-07-04 10:51:49 +0530

    Initial Commit

---
+++ Hacker News/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Hacker News
+//
+//  Created by Ashish Kapoor on 04/07/17.
+//  Copyright © 2017 Ashish Kapoor. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
