a6dfe7de6a08ba367797733e1ca3be56ad7ec27c
Ashish Kapoor <swiftobjc@gmail.com>
2017-07-05 00:21:39 +0530

    code refactoring

--- Hacker News/HNMainTBC.swift
+++ Hacker News/HNMainTBC.swift
@@ -12,38 +12,43 @@ class HNMainTBC: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tabBarItem.title = ""
-        
         viewControllers = setupStoriesTVC()
-        
-        tabBar.isTranslucent = false
-        
-        let topBorder = CALayer()
-        topBorder.frame = CGRect(x: 0, y: 0, width: 1000, height: 0.5)
-        topBorder.backgroundColor = UIColor.red.cgColor
-        
-        
-        tabBar.layer.addSublayer(topBorder)
-        tabBar.clipsToBounds = true
+        setupTabBar()
     }
     
     func setupStoriesTVC() -> [UINavigationController] {
-        // New TVC
+        
         let topStoriesNC = UINavigationController(rootViewController: HNTopTVC())
         topStoriesNC.title = kTopStory
         topStoriesNC.tabBarItem.image = UIImage(named: "topstory")
         topStoriesNC.tabBarItem.selectedImage = UIImage(named: "selected-topstory")
-        // Best TVC
+        topStoriesNC.tabBarItem.imageInsets = UIEdgeInsets(top: 8, left: 0, bottom: -8, right: 0)
+        topStoriesNC.tabBarItem.title = ""
+        
         let bestStoriesNC = UINavigationController(rootViewController: HNBestTVC())
         bestStoriesNC.title = kBestStory
         bestStoriesNC.tabBarItem.image = UIImage(named: "beststory")
         bestStoriesNC.tabBarItem.selectedImage = UIImage(named: "selected-beststory")
-        // Top TVC
+        bestStoriesNC.tabBarItem.imageInsets = UIEdgeInsets(top: 8, left: 0, bottom: -8, right: 0)
+        bestStoriesNC.tabBarItem.title = ""
+        
         let newStoriesNC = UINavigationController(rootViewController: HNNewTVC())
         newStoriesNC.title = kNewStory
         newStoriesNC.tabBarItem.image = UIImage(named: "newstory")
         newStoriesNC.tabBarItem.selectedImage = UIImage(named: "selected-newstory")
+        newStoriesNC.tabBarItem.imageInsets = UIEdgeInsets(top: 8, left: 0, bottom: -8, right: 0)
+        newStoriesNC.tabBarItem.title = ""
         
         return [topStoriesNC, bestStoriesNC, newStoriesNC]
+    }
+    
+    func setupTabBar() {
+        let topBorder = CALayer()
+        topBorder.frame = CGRect(x: 0, y: 0, width: 1000, height: 0.5)
+        topBorder.backgroundColor = UIColor.red.cgColor
+        
+        tabBar.layer.addSublayer(topBorder)
+        tabBar.clipsToBounds = true
+        tabBar.isTranslucent = false
     }
 }
